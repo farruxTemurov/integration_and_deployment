225f508a989a0b9965ef1a5e825dce53a5227a70
Farrux Temurov <tfarrux70@gmail.com>
2025-04-12 18:11:51 +0500

    simple docker image to run Java program

---
+++ Docker Images/image to run Java program/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String args[]){
+        System.out.println("Welcome to Java program");
+        int a = 10;
+        int b = 23;
+        int c = 87;
+
+        int sum = a + b + c;
+        System.out.println("Sum of 10, 23, and 87 is" + sum);
+        
+    }
+}
